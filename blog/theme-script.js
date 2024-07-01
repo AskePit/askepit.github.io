42814c70ce207629ad92eb8c21f3a9d582ebadb0
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2024-07-01 23:00:08 +0200

    dedicate common blog files

--- blog/theme-script.js
+++ blog/theme-script.js
@@ -2,6 +2,6 @@ let switcher = document.querySelector("input[name=theme]");
 
 window.onload = () => {
     switcher.addEventListener('change', function() {
-        document.getElementById('theme').href = this.checked ? 'styles_dark.css' : 'styles_light.css';
+        document.getElementById('theme').href = this.checked ? '../styles_dark.css' : '../styles_light.css';
     });
 }
