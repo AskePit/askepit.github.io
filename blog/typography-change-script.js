50687eee07c6ebbe51261c610bf50a3441168213
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2024-08-12 22:12:07 +0200

    make all blog articles typography customizable

---
+++ blog/typography-change-script.js
@@ -0,0 +1,18 @@
+let typoSwitcher = document.querySelector("input[name=typo]");
+
+typoVariants = [3, 2, 6, 7]
+typoVariantsNames = ['book', 'magazine', 'modern', 'tight']
+typo = 0
+
+function switchTypo() {
+    typoSwitcher.addEventListener('change', function() {
+        typo += 1
+        if (typo >= typoVariants.length) {
+            typo = 0
+        }
+        document.getElementById('typography').href = '../typography' + typoVariants[typo] + '.css';
+        document.getElementById('typo-capture').innerHTML = typoVariantsNames[typo];
+    });
+}
+
+window.addEventListener("load", switchTypo);
