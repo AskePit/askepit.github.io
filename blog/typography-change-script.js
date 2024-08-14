3a9fab63bc872d2eef213777cfa9078ca1426958
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2024-08-14 16:17:35 +0200

    polish typography

--- blog/typography-change-script.js
+++ blog/typography-change-script.js
@@ -2,17 +2,22 @@ let typoSwitcher = document.querySelector("input[name=typo]");
 
 typoVariants = [3, 2, 6, 7]
 typoVariantsNames = ['book', 'magazine', 'modern', 'tight']
-typo = 0
+typo = -1
 
 function switchTypo() {
-    typoSwitcher.addEventListener('change', function() {
+    function switchWork() {
         typo += 1
         if (typo >= typoVariants.length) {
             typo = 0
         }
         document.getElementById('typography').href = '../typography' + typoVariants[typo] + '.css';
         document.getElementById('typo-capture').innerHTML = typoVariantsNames[typo];
+    }
+
+    typoSwitcher.addEventListener('change', function() {
+        switchWork();
     });
+    switchWork();
 }
 
 window.addEventListener("load", switchTypo);
