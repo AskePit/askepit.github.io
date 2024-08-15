0b687aa9355f219e45ec31ea4c3f6e3a18e7086d
Nikolai Shalakin <nikolai.shalakin@lifeisfeudal.com>
2024-08-15 22:22:46 +0200

    polish typography

--- blog/typography-change-script.js
+++ blog/typography-change-script.js
@@ -1,16 +1,15 @@
 let typoSwitcher = document.querySelector("input[name=typo]");
 
-typoVariants = [3, 2, 6, 7]
 typoVariantsNames = ['book', 'magazine', 'modern', 'tight']
 typo = -1
 
 function switchTypo() {
     function switchWork() {
         typo += 1
-        if (typo >= typoVariants.length) {
+        if (typo >= typoVariantsNames.length) {
             typo = 0
         }
-        document.getElementById('typography').href = '../typography' + typoVariants[typo] + '.css';
+        document.getElementById('typography').href = '../typography_' + typoVariantsNames[typo] + '.css';
         document.getElementById('typo-capture').innerHTML = typoVariantsNames[typo];
     }
 
